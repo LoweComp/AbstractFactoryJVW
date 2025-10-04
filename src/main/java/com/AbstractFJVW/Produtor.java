a477860f9071668e237fd457cc2b7b8b0401bd80
LoweComp <terci_300@outlook.com>
2025-10-04 19:14:22 -0300

    Atributos e funcao nova ao produtor

--- src/main/java/com/AbstractFJVW/Produtor.java
+++ src/main/java/com/AbstractFJVW/Produtor.java
@@ -1,19 +1,35 @@
 package com.AbstractFJVW;
 
 public class Produtor {
+    private String nomeProdutor;
+    private String tipoFestival;
+
     private Iluminacao iluminacao;
     private EstruturaPalco estrutura;
 
-    public Produtor(FabricaProducao fabrica) {
+    public Produtor(String nomeProdutor, String tipoFestival, FabricaProducao fabrica) {
+        this.nomeProdutor = nomeProdutor;
+        this.tipoFestival = tipoFestival;
+
         this.iluminacao = fabrica.criarIluminacao();
         this.estrutura = fabrica.criarEstruturaPalco();
+    }
+
+    public String getNomeProdutor() {
+        return nomeProdutor;
+    }
+    public String getTipoFestival() {
+        return tipoFestival;
     }
 
     public String configurarIluminacao() {
         return this.iluminacao.configurarLuzes();
     }
-
     public String montarEstrutura() {
         return this.estrutura.montar();
+    }
+
+    public String iniciarProducao() {
+        return nomeProdutor + " iniciou a produção do festival de " + tipoFestival + ".";
     }
 }
